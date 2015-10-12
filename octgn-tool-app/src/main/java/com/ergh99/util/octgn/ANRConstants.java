4dbca71ef89709a3778ad83ecd76d633778bea77
David Stern <David.Stern@CapitalOne.com>
2015-10-12 17:06:10 -0500

    Refactored to use enum rather than init-on-load java.util.List

--- octgn-tool-app/src/main/java/com/ergh99/util/octgn/ANRConstants.java
+++ octgn-tool-app/src/main/java/com/ergh99/util/octgn/ANRConstants.java
@@ -1,17 +1,22 @@
 package com.ergh99.util.octgn;
 
-import java.util.Arrays;
-import java.util.List;
-
 public interface ANRConstants {
 
-	public static final String ANR_TITLE = "Android-Netrunner";
-	public static final String ANR_ID =
-			"0f38e453-26df-4c04-9d67-6d43de939c77";
-	public static final String FORMAT_EXT = ".png";
-	public static final String OLD_FORMAT_EXT = ".jpg";
-	public static final String MARKERS_SET = "Markers";
-	public static final String PROMOS_SET = "Promos";
-	public static final List<String> skippableSets =
-			Arrays.asList(MARKERS_SET, PROMOS_SET);
+    public static final String ANR_TITLE = "Android-Netrunner";
+    public static final String ANR_ID = "0f38e453-26df-4c04-9d67-6d43de939c77";
+    public static final String FORMAT_EXT = ".png";
+    public static final String OLD_FORMAT_EXT = ".jpg";
+
+    public static enum SKIPPABLE_SETS {
+        MARKERS, PROMOS;
+
+        public static boolean contains(String setName) {
+            for (SKIPPABLE_SETS set : SKIPPABLE_SETS.values()) {
+                if (set.toString().equalsIgnoreCase(setName)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 }
